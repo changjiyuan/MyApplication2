100182f149c201ccba3cb99e7dfe5ab68abb7812
changjiyuan <2661162647@qq.com>
2017-09-12 08:28:44 +0800

    Initial commit

---
+++ app/src/test/java/fileapp/ExampleUnitTest.java
@@ -0,0 +1,11 @@
+package fileapp;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
